684cde2dd103cfa3209e30866fc79ad1db69a956
Geoffrey Hunter <gbmhunter@gmail.com>
2014-11-20 17:44:58 +1300

    Added PCB track size calculator, closes #73.

---
+++ pcb-track-width/pcb-track-width.js
@@ -0,0 +1,220 @@
+//
+// @file 				pcb-track-width.js
+// @author 				Geoffrey Hunter <gbmhunter@gmail.com> (www.mbedded.ninja)
+// @edited 				n/a
+// @created				2014-11-19
+// @last-modified		2014-11-20
+// @brief 				
+// @details
+//		See the README in the root dir for more info.
+	
+
+// Adding the pcbTrackWidth "namespace" for the calculator, so that multiple calculators can work
+// on the same page. Use the data-bind="with: pcbTrackWidth" command within the HTML to access the child variables.
+function pcbTrackWidth()
+{
+
+	//! @brief		Units for resistance
+	var resistanceUnits = [
+		new cc.unit('m\u2126', 0.001),
+		new cc.unit('\u2126', 1.0),
+		new cc.unit('k\u2126', 1000.0)
+	];
+
+	//===== CURRENT (input) =====//
+
+	//! @brief		The track current (input).
+	this.current = new cc.variable({
+		name: 'current',		// Debugging name
+		app: this,
+		eqFn: function() { return; },	// Is input so don't need an equation function
+		validatorFn: function() { return true; },
+		units: [
+			new cc.unit('uA', 0.000001),
+			new cc.unit('mA', 0.001),
+			new cc.unit('A',  1.0),
+		],
+		selUnitNum: 2,
+		roundTo: 2,
+		stateFn: function() { return cc.stateEnum.input; }	// Always an input
+	});
+
+	// Add validator
+	this.current.AddValidator(cc.validatorEnum.IS_NUMBER, cc.severityEnum.error);
+	this.current.AddValidator(cc.validatorEnum.IS_POSITIVE_OR_ZERO, cc.severityEnum.error);
+	this.current.AddCustomValidator(
+		this.current,
+		'Value is above recommended maximum (35A). Equation will not be as accurate (extrapolation will occur).',
+		function(variable)
+		{
+			if(variable.val() <= 35)
+				return true;
+			else
+				return false;
+		},
+		cc.severityEnum.warning);
+	
+	//===== TEMP RISE (input) =====//
+
+	//! @brief		
+	this.tempRise = new cc.variable({
+		name: 'tempRise',
+		app: this,
+		eqFn: function() { return; },	
+		validatorFn: function() { return true; },
+		units: [
+			new cc.unit('\xB0C', 1.0)
+		],
+		selUnitNum: 0,
+		roundTo: 2,
+		stateFn: function() { return cc.stateEnum.input; }	// Always an input
+	});
+
+	// Add validator
+	this.tempRise.AddValidator(cc.validatorEnum.IS_NUMBER, cc.severityEnum.error);
+	this.tempRise.AddValidator(cc.validatorEnum.IS_POSITIVE_OR_ZERO, cc.severityEnum.error);
+	this.tempRise.AddCustomValidator(
+		this.tempRise,
+		'Value is below recommended minimum (10C). Equation will not be as accurate (extrapolation will occur)',
+		function(variable)
+		{
+			if(variable.val() >= 10)
+				return true;
+			else
+				return false;
+		},
+		cc.severityEnum.warning);
+	this.tempRise.AddCustomValidator(
+		this.tempRise,
+		'Value is above recommended maximum (100C). Equation will not be as accurate (extrapolation will occur)',
+		function(variable)
+		{
+			if(variable.val() <= 100)
+				return true;
+			else
+				return false;
+		},
+		cc.severityEnum.warning);
+	
+
+	//===== COPPER THICKNESS (input) =====//
+
+	//! @brief		
+	this.copperThickness = new cc.variable({
+		name: 'copperThickness',
+		app: this,
+		eqFn: function() { return; },	
+		validatorFn: function() { return true; },
+		units: [
+			new cc.unit('um', 0.000001),
+			new cc.unit('oz', 0.0000350012)
+		],
+		selUnitNum: 0,
+		roundTo: 2,
+		stateFn: function() { return cc.stateEnum.input; }	// Always an input
+	});
+
+	// Add validator
+	this.copperThickness.AddValidator(cc.validatorEnum.IS_NUMBER, cc.severityEnum.error);
+	this.copperThickness.AddValidator(cc.validatorEnum.IS_POSITIVE_OR_ZERO, cc.severityEnum.error);
+	this.copperThickness.AddCustomValidator(
+		this.copperThickness,
+		'Value is below recommended minimum (17.5um ot 0.5oz). Equation will not be as accurate (extrapolation will occur)',
+		function(variable)
+		{
+			if(variable.val() >= 0.0000175)
+				return true;
+			else
+				return false;
+		},
+		cc.severityEnum.warning);
+	this.copperThickness.AddCustomValidator(
+		this.copperThickness,
+		'Value is above recommended maximum (105um, or 3oz). Equation will not be as accurate (extrapolation will occur)',
+		function(variable)
+		{
+			if(variable.val() <= 0.0001050036)
+				return true;
+			else
+				return false;
+		},
+		cc.severityEnum.warning);
+	
+	//===== TRACE LOCATION (input) =====//
+
+	this.traceLocation = ko.observable("externalTrace");
+
+	//===== MINIMUM TRACK WIDTH (output) =====//
+
+	this.minimumTrackWidth = new cc.variable({
+		name: 'minimumTrackWidth',
+		app: this,
+		eqFn: function() 
+		{
+			
+			//Log('this.actualRes.eqFn() called...');
+			
+			// Quit if units have not been initialised
+			if(this.current.selUnit() === undefined)
+			{
+				//Log('this.desiredRes.selUnit() has not been defined, returning...');
+				return;
+			}
+			
+			//Log('Mulitplier = ' + this.desiredRes.selUnit().multiplier);
+			
+			// Get desired resistance  
+			var current = this.current.val();
+			Log('current = ' + current);
+			var tempRise = this.tempRise.val();
+			Log('tempRise = ' + tempRise);
+			Log('copperThickness = ' + this.copperThickness.val())
+
+			if(this.traceLocation() == 'externalTrace')     
+			{
+				Log('External trace selected.');
+				crossSectionalArea = (Math.pow((current/(0.048*Math.pow(this.tempRise.val(), 0.44))), 1/0.725));
+				Log('Cross-sectional area = ' + crossSectionalArea);
+				width = (crossSectionalArea/(this.copperThickness.val()*1000000.0/25.4))*(25.4/1000000.0);
+				return width;
+			}
+			else if(this.traceLocation() == 'internalTrace')
+			{
+				Log('Internal trace selected.');
+				crossSectionalArea = (Math.pow((current/(0.024*Math.pow(this.tempRise.val(), 0.44))), 1/0.725));
+				Log('Cross-sectional area = ' + crossSectionalArea);
+				width = (crossSectionalArea/(this.copperThickness.val()*1000000.0/25.4))*(25.4/1000000.0);
+				return width;
+			}
+		 
+	
+			//Log('Scaled resistance = ' + scaledDesRes);
+			
+			// Return the actual resistance
+			//Log('Returning closest resistance = ' + scaledDesRes);
+			//return (current*Math.pow(10, 2));
+			
+		},
+		validatorFn: function() { return true; },
+		units: [
+			new cc.unit('um', 0.000001),
+			new cc.unit('mm', 0.001)
+		],
+		selUnitNum: 0,
+		roundTo: 2,
+		stateFn: function() { return cc.stateEnum.output; }		// Always an output
+	});	
+
+	// Function calculates the base-10 log of the given input
+	function Log10(val)
+	{
+		// Use rule log10(x) = ln(x)/ln(10)
+		return Math.log(val) / Math.LN10;
+	}
+
+
+}
+
+// Register the calculator
+cc.registerCalc(pcbTrackWidth, 'pcbTrackWidth');
+
---
+++ pcb-track-width/pcb-track-width.php
@@ -0,0 +1,112 @@
+<!--
+// @file 			pcb-track-width.php
+// @author 			Geoffrey Hunter <gbmhunter@gmail.com> (www.mbedded.ninja)
+// @edited 			n/a
+// @date 			2014-11-19
+// @last-modified	2014-11-19
+// @brief 			This calculator can find the minimum PCB track width (external or internal layer) given the track current, the allowed temperatur rise, and copper layer thickness. Calculated in accordance with IPC-2221A.
+// @details
+//		See the README in the root dir for more info.
+-->
+
+<script type="text/javascript">
+	window.jQuery || document.write('<script src="http://code.jquery.com/jquery-latest.min.js"><\/script>')
+</script>
+
+<!-- candy-calc logic -->
+<script type="text/javascript" src="/candy-calc/js/candy-calc.js" ></script>
+<!-- candy-calc CSS file -->
+<link type="text/css" rel="stylesheet" href="/candy-calc/css/candy-calc.css" />
+
+<p>This calculator can find the minimum PCB track width (external or internal layer) given the track current, the allowed temperatur rise, and copper layer thickness.</p>
+
+<p>Calculated in accordance with the equations in IPC-2221A Section 6.2 (formerly IPC-D-275, the equation has not changed between these two standards amd you can get similar values by curve-fitting to the graphs provided in IPC-D-275, drawn in 1954, woah!).</p>
+
+<p>$$ I = k\Delta T^b A^c $$</p>
+<p style="text-align: center;">
+	where:<br>
+	\( k \) = 0.048 for external traces, 0.024 for internal tracks<br>
+	\( \Delta T \) = the change in temperature (temperature rise) in \( ^{\circ}C \)<br>
+	\( b \) = 0.44<br>
+	\( A \) = cross-sectional area in \( mils^2 \)<br>
+	\( c \) = 0.725
+</p>
+
+<p>The standard only covers values where the current is 0-35A, track width is 0-10.16mm, temperature rise is from 10-100C, and the copper from 0.5-3oz. Values outside this range are extrapolated (and there more error-prone) and will turn orange.</p>
+
+<p>This also assumes the track is sufficiently long enough the the end-points do not have a significant effect on the heatsinking. For example, this calcultor should not be used for calculating the width of thermal-relief style connections from a copper pour to a via, in where the track is very short (0.2-1.0mm). It also assumes there are no vias along the length of the track.</p>
+
+<p>The current in assumed to be constant (DC). However, you can use the RMS value for a pulsed current as long as the pulses are fast enough.</p>
+
+<p>The temperature of the PCB material should NEVER exceed the relative thermal index (RTI) of the material. This is defined in UL746B as the temperature at which 50% of the materials properties are retained after 100,000 hours.</p>
+
+<p>Remember this calculator does not take into account other nearby heat sources.</p>
+
+<table id="pcbTrackWidth" class="candy-calc" border="4" style="width: 500px;">
+	<tbody>
+		<tr>
+			<td><strong>Variable:</strong></td>
+			<td><strong>Value:</strong></td>
+			<td><strong>Units:</strong></td>	
+		</tr>
+		<tr>
+			<td class="symbol">Trace Current</td>
+			<td>
+				<input class="input" data-bind="calcVar: current, valueUpdate: 'afterkeydown'" size="16" ></input>
+			</td>
+			<td>
+				<select data-bind="options: current.units, optionsText: 'name', value: current.selUnit"></select>
+			</td>			
+		</tr>
+		<tr>
+			<td class="symbol">Temp Rise</td>
+			<td>
+				<input class="input" data-bind="calcVar: tempRise, valueUpdate: 'afterkeydown'" size="16" ></input>
+			</td>
+			<td>
+				<select data-bind="options: tempRise.units, optionsText: 'name', value: tempRise.selUnit"></select>
+			</td>			
+		</tr>	
+		<tr>
+			<td class="symbol">Copper Thickness</td>
+			<td>
+				<input class="input" data-bind="calcVar: copperThickness, valueUpdate: 'afterkeydown'" size="16" ></input>
+			</td>
+			<td>
+				<select data-bind="options: copperThickness.units, optionsText: 'name', value: copperThickness.selUnit"></select>
+			</td>			
+		</tr>
+		<tr>
+			<td colspan="3" style="text-align: center;">
+				<input data-bind="checked: traceLocation" type="radio" name="range" value="externalTrace">External Trace   </input>
+				<input data-bind="checked: traceLocation" type="radio" name="range" value="internalTrace">Internal Trace   </input>
+			</td>
+		</tr>		
+		<tr>
+			<td class="symbol">Minimum Track Width</td>
+			<td>
+				<input class="output" data-bind="calcVar: minimumTrackWidth, valueUpdate: 'afterkeydown'" type="text" size="16" />
+			</td>
+			<td>
+				<select data-bind="options: minimumTrackWidth.units, optionsText: 'name', value: minimumTrackWidth.selUnit"></select>
+			</td>
+		</tr>	
+	</tbody>
+</table>
+<!-- Include Javascript file for calculator. Path is built from this scripts path, using __FILE__ variable. -->
+<?php
+
+	// Get full path
+	$cur_file = str_replace('\\','/', realpath(dirname(__FILE__)));
+	// Remove everything up to public_html (Apache) or htdocs (xampp)
+	$cur_file = preg_replace('/(.*?)\/public_html/', '', $cur_file);
+	$cur_file = preg_replace('/(.*?)\/htdocs/', '', $cur_file);
+	// Output HTML
+	echo '<script type="text/javascript" src="' . $cur_file . '\pcb-track-width.js"></script>';
+
+	?>
+
+<!--
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+-->
+
